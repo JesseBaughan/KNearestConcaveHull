3816fff253f8e8aef006d52d68a4a6bafb9c839a
JesseBaughan <jesse.baughan@gmail.com>
2023-06-03 09:47:32 +1000

    First commit

---
+++ KmeansConcaveHull/KMeansConcaveHull.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file KMeansConcaveHull.cpp
+ *
+ * @brief A description of the module’s purpose.
+ *
+ * @author Name - email address
+ *
+ * @par 
+ *
+ */
+ 
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "KMeansConcaveHull.hpp"
+
+/*** end of file ***/
---
+++ KmeansConcaveHull/KMeansConcaveHull.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file KMeansConcaveHull.hpp
+ *
+ * @brief A description of the module’s purpose.
+ *
+ * @author Name - email address
+ *
+ * @par 
+ *
+ */
+ 
+#ifndef KMEANS_CONCAVE_HULL_H
+#define KMEANS_CONCAVE_HULL_H
+
+#endif /* KMEANS_CONCAVE_HULL_H */
+
+/*** end of file ***/
